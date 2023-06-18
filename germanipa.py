6a6d006854e1ef6c92147467f8f582845ec77754
Christian-W. Budde <Christian@pcjv.de>
2023-06-19 01:36:29 +0200

    Added command line input

--- germanipa.py
+++ germanipa.py
@@ -1,6 +1,13 @@
+import sys
 from text import Text
 
 if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        text_input = sys.argv[1]
+        b = Text(text_input)
+        b.print_ipa()
+        sys.exit(1)
+
     while True:
         sentinel = '/'  # ends when this string is seen
         print('Enter a German text below.\nThen type "/" and hit enter.\n')
